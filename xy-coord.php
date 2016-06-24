33232490897bb748016d9dbba472fe66cc29641d
Joseph Wolf <jswolf1223@gmail.com>
2016-06-24 11:59:06 -0400

    More documentation on data visualisation.

--- xy-coord.php
+++ xy-coord.php
@@ -2,7 +2,9 @@
 
 /*  Finds central emotion of an image. The x position is found by adding the Plutchik wheel's right emotions' averages and
  *  subtracting the left emotions' averages from x.The y position is found by adding the Plutchik wheel's top emotions'
- *  averages and subtracting the bottom emotions' averages from y.  */ 
+ *  averages and subtracting the bottom emotions' averages from y. x andy Coordinates are used to with emotion wheel data
+ *  visualisation
+ */ 
 function getCoords($coords) {
 	$x = ($coords ['trust'] / 2) + $coords ['fear'] + ($coords ['surprise'] / 2) - $coords ['anger'] - 
 		($coords ['disgust'] / 2) - ($coords ['anticipation'] / 2);
